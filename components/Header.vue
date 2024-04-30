95a60591761fcfdb2b1a0c68556cf98d3a6c7fc1
Mhmmdneil <141992443+Mhmmdneil@users.noreply.github.com>
2024-04-30 10:21:30 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row p-3 bg-primary text-black">
+  <div class="col-lg-1">
+    <img src="~/assets/img/LOGO-SMK4.png" alt="logo smkn 4">
+  </div>
+
+  <div class="col-lg-11">
+    <h1>Perpus Digital</h1>
+    <address>
+      SMKN 4 Tasikmalaya
+      Jl,Depok, Sukamenak, Purbaratu
+    </address>
+  </div>
+  </div>
+</template>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
+        </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>Muhammad Neil</td>
+              <td>Siswa</td>
+              <td>29 April 2024, 24.32.00</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
